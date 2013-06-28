2ee1c1ab5af984c5b7fff88e7c2dc2fb88d431c3
Miles Steele <msteele@edx.org>
2013-06-28 13:46:31 -0400

    add tests

--- lms/djangoapps/instructor/tests/test_access.py
+++ lms/djangoapps/instructor/tests/test_access.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from nose.tools import raises
 from django.contrib.auth.models import User, Group
 from student.tests.factories import UserFactory
 from xmodule.modulestore.tests.factories import CourseFactory
@@ -10,7 +11,11 @@ from courseware.tests.tests import mongo_store_config, xml_store_config
 
 from student.models import CourseEnrollment, CourseEnrollmentAllowed
 from courseware.access import get_access_group_name
-from instructor.access import allow_access, revoke_access
+from django_comment_common.models import (Role,
+                                          FORUM_ROLE_ADMINISTRATOR,
+                                          FORUM_ROLE_MODERATOR,
+                                          FORUM_ROLE_COMMUNITY_TA)
+from instructor.access import allow_access, revoke_access, list_with_level, update_forum_role_membership
 
 # mock dependency
 # get_access_group_name = lambda course, role: '{0}_{1}'.format(course.course_id, role)
@@ -100,9 +105,88 @@ class TestInstructorAccessControlDB(ModuleStoreTestCase):
         self.assertIn(users[2], Group.objects.get(name=get_access_group_name(self.course, levels[2])).user_set.all())
 
 
-    # def test_allow_disallow_multirole(self):
+@override_settings(MODULESTORE=TEST_DATA_MONGO_MODULESTORE)
+class TestInstructorAccessControlPrefilledDB(ModuleStoreTestCase):
+    def setUp(self):
+        self.course = CourseFactory.create()
+
+        # setup instructors
+        self.instructors = set([UserFactory.create(), UserFactory.create()])
+        [allow_access(self.course, user, 'instructor') for user in self.instructors]
+
+    def test_list_with_level(self):
+        instructors = set(list_with_level(self.course, 'instructor'))
+        self.assertEqual(instructors, self.instructors)
+
+    def test_list_with_level_not_yet_group(self):
+        instructors = set(list_with_level(self.course, 'staff'))
+        self.assertEqual(instructors, set())
+
+    def test_list_with_level_bad_group(self):
+        self.assertEqual(set(list_with_level(self.course, 'robot-definitely-not-a-group')), set())
+
+    def test_list_with_level_beta(self):
+        beta_testers_result = set(list_with_level(self.course, 'beta'))
+        self.assertEqual(set(), beta_testers_result)
+
+        beta_testers = set([UserFactory.create(), UserFactory.create()])
+        [allow_access(self.course, user, 'beta') for user in beta_testers]
+        beta_testers_result = set(list_with_level(self.course, 'beta'))
+        self.assertEqual(beta_testers, beta_testers_result)
 
 
-class MockCourse(object):
-    def __init__(self, course_id):
-        self.course_id = course_id
+@override_settings(MODULESTORE=TEST_DATA_MONGO_MODULESTORE)
+class TestInstructorAccessForumDB(ModuleStoreTestCase):
+    def setUp(self):
+        self.course = CourseFactory.create()
+
+        self.moderators = set([UserFactory.create() for _ in xrange(4)])
+        self.mod_role = Role.objects.create(course_id=self.course.id, name=FORUM_ROLE_MODERATOR)
+        [self.mod_role.users.add(user) for user in self.moderators]
+
+    def test_update_forum_role_membership_allow_existing_role(self):
+        user = UserFactory.create()
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_MODERATOR, 'allow')
+        self.assertIn(user, self.mod_role.users.all())
+
+    def test_update_forum_role_membership_allow_existing_role_allowed_user(self):
+        user = UserFactory.create()
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_MODERATOR, 'allow')
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_MODERATOR, 'allow')
+        self.assertIn(user, self.mod_role.users.all())
+
+    @raises(Role.DoesNotExist)
+    def test_update_forum_role_membership_allow_not_existing_role(self):
+        user = UserFactory.create()
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_COMMUNITY_TA, 'allow')
+
+    def test_update_forum_role_membership_revoke_existing_role(self):
+        user = iter(self.moderators).next()
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_MODERATOR, 'revoke')
+        self.assertNotIn(user, self.mod_role.users.all())
+
+    def test_update_forum_role_membership_revoke_existing_role_revoked_user(self):
+        user = iter(self.moderators).next()
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_MODERATOR, 'revoke')
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_MODERATOR, 'revoke')
+        self.assertNotIn(user, self.mod_role.users.all())
+
+    @raises(Role.DoesNotExist)
+    def test_update_forum_role_membership_revoke_not_existing_role(self):
+        user = iter(self.moderators).next()
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_COMMUNITY_TA, 'revoke')
+
+    @raises(Role.DoesNotExist)
+    def test_update_forum_role_membership_bad_role_allow(self):
+        user = UserFactory.create()
+        update_forum_role_membership(self.course.id, user, 'robot-definitely-not-a-forum-role', 'allow')
+
+    @raises(Role.DoesNotExist)
+    def test_update_forum_role_membership_bad_role_revoke(self):
+        user = UserFactory.create()
+        update_forum_role_membership(self.course.id, user, 'robot-definitely-not-a-forum-role', 'revoke')
+
+    @raises(ValueError)
+    def test_update_forum_role_membership_bad_mode(self):
+        user = iter(self.moderators).next()
+        update_forum_role_membership(self.course.id, user, FORUM_ROLE_MODERATOR, 'robot-not-a-mode')
--- lms/djangoapps/instructor/tests/test_enrollment.py
+++ lms/djangoapps/instructor/tests/test_enrollment.py
@@ -1,8 +1,8 @@
-'''
-Unit tests for enrollment methods in views.py
+"""
+Unit tests for instructor.enrollment methods.
+"""
 
-'''
-
+import json
 from django.test.utils import override_settings
 from django.contrib.auth.models import Group, User
 from django.core.urlresolvers import reverse
@@ -10,18 +10,18 @@ from courseware.access import _course_staff_group_name
 from courseware.tests.tests import LoginEnrollmentTestCase, TEST_DATA_XML_MODULESTORE, get_user
 from xmodule.modulestore.django import modulestore
 from student.models import CourseEnrollment, CourseEnrollmentAllowed
-from instructor.views.legacy import get_and_clean_student_list
+from courseware.models import StudentModule
 from django.test import TestCase
 from student.tests.factories import UserFactory
 
 from student.models import CourseEnrollment, CourseEnrollmentAllowed
-from instructor.enrollment import enroll_emails, unenroll_emails, split_input_list
+from instructor.enrollment import enroll_emails, unenroll_emails, split_input_list, reset_student_attempts
 
 
 class TestInstructorEnrollmentDB(TestCase):
     '''Test instructor enrollment administration against database effects'''
     def setUp(self):
-        self.course = MockCourse('jus:/a/fake/c::rse/id')
+        self.course_id = 'robot:/a/fake/c::rse/id'
 
     def test_split_input_list(self):
         strings = []
@@ -34,52 +34,52 @@ class TestInstructorEnrollmentDB(TestCase):
 
     def test_enroll_emails_userexists_alreadyenrolled(self):
         user = UserFactory()
-        ce = CourseEnrollment(course_id=self.course.course_id, user=user)
+        ce = CourseEnrollment(course_id=self.course_id, user=user)
         ce.save()
 
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=user.email).count(), 1)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=user.email).count(), 1)
 
-        enroll_emails(self.course.course_id, [user.email])
+        enroll_emails(self.course_id, [user.email])
 
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=user.email).count(), 1)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=user.email).count(), 1)
 
     def test_enroll_emails_userexists_succeedenrolling(self):
         user = UserFactory()
 
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=user.email).count(), 0)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=user.email).count(), 0)
 
-        enroll_emails(self.course.course_id, [user.email])
+        enroll_emails(self.course_id, [user.email])
 
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=user.email).count(), 1)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=user.email).count(), 1)
 
     def test_enroll_emails_nouser_alreadyallowed(self):
         email_without_user = 'test_enroll_emails_nouser_alreadyallowed@test.org'
 
         self.assertEqual(User.objects.filter(email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=email_without_user).count(), 0)
 
-        cea = CourseEnrollmentAllowed(course_id=self.course.course_id, email=email_without_user, auto_enroll=False)
+        cea = CourseEnrollmentAllowed(course_id=self.course_id, email=email_without_user, auto_enroll=False)
         cea.save()
 
-        enroll_emails(self.course.course_id, [email_without_user])
+        enroll_emails(self.course_id, [email_without_user])
 
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=email_without_user).count(), 1)
-        self.assertEqual(CourseEnrollmentAllowed.objects.get(course_id=self.course.course_id, email=email_without_user).auto_enroll, False)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=email_without_user).count(), 1)
+        self.assertEqual(CourseEnrollmentAllowed.objects.get(course_id=self.course_id, email=email_without_user).auto_enroll, False)
 
     def test_enroll_emails_nouser_suceedallow(self):
         email_without_user = 'test_enroll_emails_nouser_suceedallow@test.org'
 
         self.assertEqual(User.objects.filter(email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=email_without_user).count(), 0)
 
-        enroll_emails(self.course.course_id, [email_without_user])
+        enroll_emails(self.course_id, [email_without_user])
 
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=email_without_user).count(), 1)
-        self.assertEqual(CourseEnrollmentAllowed.objects.get(course_id=self.course.course_id, email=email_without_user).auto_enroll, False)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=email_without_user).count(), 1)
+        self.assertEqual(CourseEnrollmentAllowed.objects.get(course_id=self.course_id, email=email_without_user).auto_enroll, False)
 
     def test_enroll_multiple(self):
         user1 = UserFactory()
@@ -91,58 +91,73 @@ class TestInstructorEnrollmentDB(TestCase):
 
         def test_db(auto_enroll):
             for user in [user1, user2, user3]:
-                self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user=user).count(), 1)
-                self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=user.email).count(), 0)
+                self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user=user).count(), 1)
+                self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=user.email).count(), 0)
 
             for email in [email_without_user1, email_without_user2, email_without_user3]:
-                self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=email).count(), 0)
-                self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=email).count(), 1)
-                self.assertEqual(CourseEnrollmentAllowed.objects.get(course_id=self.course.course_id, email=email).auto_enroll, auto_enroll)
+                self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=email).count(), 0)
+                self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=email).count(), 1)
+                self.assertEqual(CourseEnrollmentAllowed.objects.get(course_id=self.course_id, email=email).auto_enroll, auto_enroll)
 
-        enroll_emails(self.course.course_id, [user1.email, user2.email, user3.email, email_without_user1, email_without_user2, email_without_user3], auto_enroll=True)
+        enroll_emails(self.course_id, [user1.email, user2.email, user3.email, email_without_user1, email_without_user2, email_without_user3], auto_enroll=True)
         test_db(True)
-        enroll_emails(self.course.course_id, [user1.email, user2.email, user3.email, email_without_user1, email_without_user2, email_without_user3], auto_enroll=False)
+        enroll_emails(self.course_id, [user1.email, user2.email, user3.email, email_without_user1, email_without_user2, email_without_user3], auto_enroll=False)
         test_db(False)
 
     def test_unenroll_alreadyallowed(self):
         email_without_user = 'test_unenroll_alreadyallowed@test.org'
-        cea = CourseEnrollmentAllowed(course_id=self.course.course_id, email=email_without_user, auto_enroll=False)
+        cea = CourseEnrollmentAllowed(course_id=self.course_id, email=email_without_user, auto_enroll=False)
         cea.save()
 
-        unenroll_emails(self.course.course_id, [email_without_user])
+        unenroll_emails(self.course_id, [email_without_user])
 
         self.assertEqual(User.objects.filter(email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=email_without_user).count(), 0)
 
     def test_unenroll_alreadyenrolled(self):
         user = UserFactory()
-        ce = CourseEnrollment(course_id=self.course.course_id, user=user)
+        ce = CourseEnrollment(course_id=self.course_id, user=user)
         ce.save()
 
-        unenroll_emails(self.course.course_id, [user.email])
+        unenroll_emails(self.course_id, [user.email])
 
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user=user).count(), 0)
-        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=user.email).count(), 0)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user=user).count(), 0)
+        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=user.email).count(), 0)
 
     def test_unenroll_notenrolled(self):
         user = UserFactory()
 
-        unenroll_emails(self.course.course_id, [user.email])
+        unenroll_emails(self.course_id, [user.email])
 
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user=user).count(), 0)
-        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=user.email).count(), 0)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user=user).count(), 0)
+        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=user.email).count(), 0)
 
     def test_unenroll_nosuchuser(self):
         email_without_user = 'test_unenroll_nosuchuser@test.org'
 
-        unenroll_emails(self.course.course_id, [email_without_user])
+        unenroll_emails(self.course_id, [email_without_user])
 
         self.assertEqual(User.objects.filter(email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course.course_id, user__email=email_without_user).count(), 0)
-        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course.course_id, email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollment.objects.filter(course_id=self.course_id, user__email=email_without_user).count(), 0)
+        self.assertEqual(CourseEnrollmentAllowed.objects.filter(course_id=self.course_id, email=email_without_user).count(), 0)
 
+    def test_reset_student_attempts(self):
+        user = UserFactory()
+        msk = 'robot/module/state/key'
+        original_state = json.dumps({'attempts': 32, 'otherstuff': 'alsorobots'})
+        module = StudentModule.objects.create(student=user, course_id=self.course_id, module_state_key=msk, state=original_state)
+        # lambda to reload the module state from the database
+        module = lambda: StudentModule.objects.get(student=user, course_id=self.course_id, module_state_key=msk)
+        self.assertEqual(json.loads(module().state)['attempts'], 32)
+        reset_student_attempts(self.course_id, user, msk)
+        self.assertEqual(json.loads(module().state)['attempts'], 0)
 
-class MockCourse(object):
-    def __init__(self, course_id):
-        self.course_id = course_id
+    def test_delete_student_attempts(self):
+        user = UserFactory()
+        msk = 'robot/module/state/key'
+        original_state = json.dumps({'attempts': 32, 'otherstuff': 'alsorobots'})
+        StudentModule.objects.create(student=user, course_id=self.course_id, module_state_key=msk, state=original_state)
+        self.assertEqual(StudentModule.objects.filter(student=user, course_id=self.course_id, module_state_key=msk).count(), 1)
+        reset_student_attempts(self.course_id, user, msk, delete_module=True)
+        self.assertEqual(StudentModule.objects.filter(student=user, course_id=self.course_id, module_state_key=msk).count(), 0)
